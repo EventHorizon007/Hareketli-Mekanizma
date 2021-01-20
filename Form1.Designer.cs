e83ac513f01d2025ca661c9574ccc952c2974455
emre <emre@DESKTOP-5ODF2G4>
2021-01-20 21:29:19 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Hareketli_Mekanizma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(675, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Red;
+            this.trackBar1.Location = new System.Drawing.Point(650, 50);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(150, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(675, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hareketli_Mekanizma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //1.CizimAlani tanımlanıyor mavi dikdörtgenler haric hersey buraya cizdirilecek.
+        Graphics CizimAlani;
+        Pen Kalem1 = new Pen(System.Drawing.Color.Yellow, 1);
+        Pen Kalem2 = new Pen(System.Drawing.Color.Red, 1);
+        //Mavi dikdörtgenleri buraya cizdiricez.Çünkü bu alan döndürülerek mavi çubuğun dönmesi sağlanacak.
+        Graphics CizimAlani2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Cizim alanlarının picturebox üzerinde görüntülenmesi ve timer value nun textbox a atanması.
+            CizimAlani = pictureBox1.CreateGraphics();
+            CizimAlani2 = pictureBox1.CreateGraphics();
+            textBox1.Text = timer1.Interval.ToString();
+        }
+        int x, y;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //butona tıklandığında timer çalışacak.
+            timer1.Start();
+            
+        }
+
+        //Döndürme islemi bu class ile yapılıyor genislik ve yükseklik 2 ye bölünürse ortadan döner.
+        //Su an dikdörtgenin ucundan dönüyör.
+        public void RotateRectangle(Graphics g, Rectangle r, float angle)
+        {
+            using (Matrix m = new Matrix())
+            {
+                m.RotateAt(angle, new PointF(r.X + (r.Width), r.Y + (r.Height)));
+                CizimAlani2.Transform = m;
+                CizimAlani2.DrawRectangle(Pens.Blue, r);
+                CizimAlani2.ResetTransform();
+            }
+        }
+        //sayaclar ve daire dilimin baslangıc acısı.Dısarda tanımlanmalarının nedeni global olmaları gerektiginden.
+        int sayac = -1;
+        int sayac1 =0;
+        float startAngle = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //bu sayac sartı degerleri sistemdeki parcaların kat etmesi gereken 
+            //acılar ve mesafelerin ebob una göre hesaplandı. 
+            if (sayac < 15)
+            { 
+                sayac++; 
+            }
+
+            if (sayac1 < 160)
+            {
+                sayac1+=10;
+            }
+
+            x = 75;
+            y = 350;
+            
+            pictureBox1.Refresh();
+            //Sabitleme aparatı 1
+            CizimAlani.DrawRectangle(Kalem1, x, y, 25, 20);
+            CizimAlani.DrawRectangle(Kalem1, x, y + 20, 25, 40);
+            CizimAlani.DrawRectangle(Kalem1, x, y + 60, 25, 20);
+            CizimAlani.DrawEllipse(Kalem1, (float)(x + 8.25), y + 6, 8, 8);
+            CizimAlani.DrawEllipse(Kalem1, (float)(x + 8.25), y + 66, 8, 8);
+
+            //Sabitleme aparatı 2
+            CizimAlani.DrawRectangle(Kalem1, x + 470, y, 25, 20);
+            CizimAlani.DrawRectangle(Kalem1, x + 470, y + 20, 25, 40);
+            CizimAlani.DrawRectangle(Kalem1, x + 470, y + 60, 25, 20);
+            CizimAlani.DrawEllipse(Kalem1, (float)(x + 478.25), y + 6, 8, 8);
+            CizimAlani.DrawEllipse(Kalem1, (float)(x + 478.25), y + 66, 8, 8);
+
+            //Ray çubugu
+            if (sayac <= 5)
+            {
+                CizimAlani.DrawRectangle(Kalem2, -5 - (sayac * 15), y + 25, 655, 30);
+            }
+            else if (6 <= sayac && sayac <= 15)
+            { 
+                CizimAlani.DrawRectangle(Kalem2, -5 + ((sayac - 10) * 15), y + 25, 655, 30); 
+            }
+            //Sarkac
+            CizimAlani.DrawEllipse(Kalem1, (float)307.5, (float)247.5, 35, 35);
+            CizimAlani.DrawEllipse(Kalem1, (float)297.5, (float)237.5, 55, 55);
+            Rectangle rect1 = new Rectangle(225, 152, 200, 200);
+            Rectangle rect2 = new Rectangle(250, 177, 150, 150);
+            
+            if (sayac <= 5)
+            {
+                startAngle = 30 + (sayac * 8);
+            }
+            else if (6 <= sayac)
+            {
+                startAngle = 30 - ((sayac-10) * 8);
+            }
+            
+            float sweepAngle = 120;
+            CizimAlani.DrawPie(Kalem1, rect1, startAngle, sweepAngle);
+            CizimAlani.DrawPie(Kalem1, rect2, startAngle, sweepAngle);
+
+            //Ray cubugu dislileri
+            int x1 = 470, y1 = 375, artım = 0,artım2=0;
+            if (sayac <= 5)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    CizimAlani.DrawLine(Kalem1, x1 - artım - (sayac * 15), y1, x1 - 5 - artım - (sayac * 15), y1 - 20);
+                    CizimAlani.DrawLine(Kalem1, x1 - 5 - artım - (sayac * 15), y1 - 20, x1 - 15 - artım - (sayac * 15), y1 - 20);
+                    CizimAlani.DrawLine(Kalem1, x1 - 15 - artım - (sayac * 15), y1 - 20, x1 - 20 - artım - (sayac * 15), y1);
+                    artım += 30;
+                }
+            }
+
+            else if (6<=sayac && sayac <= 15)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    CizimAlani.DrawLine(Kalem1, x1 - artım2 + ((sayac - 10) * 15), y1, x1 - 5 - artım2 + ((sayac - 10) * 15), y1 - 20);
+                    CizimAlani.DrawLine(Kalem1, x1 - 5 - artım2 + ((sayac - 10) * 15), y1 - 20, x1 - 15 - artım2 + ((sayac - 10) * 15), y1 - 20);
+                    CizimAlani.DrawLine(Kalem1, x1 - 15 - artım2 + ((sayac - 10) * 15), y1 - 20, x1 - 20 - artım2 + ((sayac - 10) * 15), y1);
+                    artım2 += 30;
+                }
+            }
+
+            //Cark
+            CizimAlani.DrawEllipse(Kalem1, 310, 120, 30, 30);
+            CizimAlani.DrawEllipse(Kalem1, 300, 110, 50, 50);
+            CizimAlani.DrawEllipse(Kalem1, 225, 35, 200, 200);
+            double a1 = 325;
+            int b1 = 135;
+            int cizgiboyu = 75;
+            CizimAlani2.SmoothingMode = SmoothingMode.HighQuality;
+            Rectangle r1 = new Rectangle(310, 10, 30, 275);
+            Rectangle r2 = new Rectangle(305, 5, 40, 285);
+            if (sayac <= 5)
+            {
+                double alfaradyan = ((sayac * 25) + 90) * 2 * Math.PI / 360;//Dereceden Radyana
+                double x2 = a1 - cizgiboyu * Math.Cos(alfaradyan);
+                double y2 = b1 - cizgiboyu * Math.Sin(alfaradyan);
+                CizimAlani.DrawEllipse(Kalem1, (float)x2 - 15, (float)y2 - 15, 30, 30);
+
+                //bu dondurme acıları deneme yanılmayla bulundu.Mavi dikdörtgenlerin kodu.
+                if (sayac1 == 40)
+                {
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 10) * 0.785));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 10) * 0.785));
+                }
+                else if (sayac1 == 50)
+                {
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 10) * 0.715));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 10) * 0.715));
+
+                }
+                else if (sayac1 <= 30)
+                {
+
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 10) * 0.780));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 10) * 0.780));
+                }
+                else if (sayac1 == 60)
+                {
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 10) * 0.625));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 10) * 0.625));
+                }
+            }
+            else if (6 <= sayac && sayac <= 15)
+            {
+                double alfaradyan = -(((sayac-10) * 25) + 90) * 2 * Math.PI / 360;//Dereceden Radyana
+                double x2 = a1 + cizgiboyu * Math.Cos(alfaradyan);
+                double y2 = b1 + cizgiboyu * Math.Sin(alfaradyan);
+                CizimAlani.DrawEllipse(Kalem1, (float)x2 - 15, (float)y2 - 15, 30, 30);
+
+                //bu dondurme acıları deneme yanılmayla bulundu.Mavi dikdörtgenlerin kodu.
+                if (sayac1 == 100)
+                {
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 110) * -0.785));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 110) * -0.785));
+                }
+                else if (sayac1 == 110 || sayac1 == 150)
+                {
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 110) * -0.715));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 110) * -0.715));
+
+                }
+
+                else if (sayac1 <= 90 || (sayac1 >= 120 && sayac1 <= 140))
+                {
+
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 110) * -0.780));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 110) * -0.780));
+                }
+                
+
+                else if (sayac1 == 160)
+                {
+
+                    RotateRectangle(CizimAlani2, r1, (float)((sayac1 - 110) * -0.625));
+                    RotateRectangle(CizimAlani2, r2, (float)((sayac1 - 110) * -0.625));
+                }
+
+            }
+
+            if (sayac == 15)
+            {
+                sayac = -1;
+                sayac1 = 0;
+            }
+
+        }
+        //trackbar ayarı.
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+            textBox1.Text = timer1.Interval.ToString();
+        }
+    }
+}
